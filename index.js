58a285ed67a4bb7f6fbe9903911933c4d73afcbf
Abdelrahman Samhan <samhanion98@gmail.com>
2022-02-26 18:02:32 +0200

    fixed the port bug

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 const expres = require("express");
 const app = expres();
 
+let port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   // 0x44DD1abdA1bC003e073c5CA21BdCAB4EA91D9531
   //   galaxy celery fabric roof poem team hurt flavor wrap proud index choose
@@ -15,6 +17,6 @@ app.get("/", (req, res) => {
   res.send(wallet.address);
 });
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
