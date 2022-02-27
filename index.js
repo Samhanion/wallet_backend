dce525fb9df9ecb2324c158bf953c9ab4b88ff96
Abdelrahman Samhan <samhanion98@gmail.com>
2022-02-27 09:29:19 +0200

    added create wallet endpoint

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const app = expres();
 
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/getAddress", (req, res) => {
   // 0x44DD1abdA1bC003e073c5CA21BdCAB4EA91D9531
   //   galaxy celery fabric roof poem team hurt flavor wrap proud index choose
-  //   console.log(req.query.mnemonic);
   let mnemonic = req.query.mnemonic;
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  console.log(wallet.address);
 
   res.send(wallet.address);
+});
+
+app.get("/createWallet", (req, res) => {
+  let wallet = ethers.Wallet.createRandom();
+  let response = {
+    address: wallet.address,
+    mnemonic: wallet.mnemonic.phrase,
+    privateKey: wallet.privateKey,
+  };
+  res.send(response);
 });
 
 app.listen(port, () => {
