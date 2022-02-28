089ec3e5f1dd79879b782edafb7fb80c2ba35a19
Abdelrahman Samhan <samhanion98@gmail.com>
2022-02-28 21:10:40 +0200

    added sendTransaction

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { ethers } = require("ethers");
 var fs = require("fs");
 
-const expres = require("express");
-const app = expres();
+const express = require("express");
+const app = express();
 
 let port = process.env.PORT || 3000;
 
@@ -23,6 +23,35 @@ app.get("/createWallet", (req, res) => {
     privateKey: wallet.privateKey,
   };
   res.send(response);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// http://localhost:3000/sendTransaction/?mnemonic=toilet skate together scheme shaft answer elder fence wasp reflect dawn paper&network=eth/ropsten&recipient=0x44DD1abdA1bC003e073c5CA21BdCAB4EA91D9531&amount=0.0001
+// send transaction
+app.get("/sendTransaction", async (req, res) => {
+  // https://speedy-nodes-nyc.moralis.io/47ef2509ed164f0635b5048e/eth/ropsten
+  //
+  const connection = new ethers.providers.JsonRpcProvider(`https://speedy-nodes-nyc.moralis.io/47ef2509ed164f0635b5048e/${req.query.network}`);
+  const gasPrice = connection.getGasPrice();
+  const wallet = ethers.Wallet.fromMnemonic(req.query.mnemonic);
+
+  const signer = wallet.connect(connection);
+  const recipient = req.query.recipient;
+  const tx = {
+    from: wallet.address,
+    to: recipient,
+    value: ethers.utils.parseEther(req.query.amount),
+    gasPrice: gasPrice,
+    gasLimit: ethers.utils.hexlify(100000),
+    nonce: connection.getTransactionCount(wallet.address, "latest"),
+  };
+  const transaction = await signer.sendTransaction(tx);
+  console.log(transaction);
+  const receipt = await transaction.wait();
+  console.log(receipt);
+  res.send(receipt);
 });
 
 app.listen(port, () => {
