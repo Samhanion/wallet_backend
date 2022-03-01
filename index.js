22672d12c4537c9b2a294c39d77ddda3a81dd8de
Abdelrahman Samhan <samhanion98@gmail.com>
2022-03-01 21:29:18 +0200

    added token transaction

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/createWallet", (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// http://localhost:3000/sendTransaction/?mnemonic=toilet skate together scheme shaft answer elder fence wasp reflect dawn paper&network=eth/ropsten&recipient=0x44DD1abdA1bC003e073c5CA21BdCAB4EA91D9531&amount=0.0001
+// http://localhost:3000/sendTransaction/?mnemonic=toilet skate together scheme shaft answer elder fence wasp reflect dawn paper&network=eth/rinkeby&recipient=0x44DD1abdA1bC003e073c5CA21BdCAB4EA91D9531&amount=1&contractAddress=0x814f11f6bd1a717b21849da13553d457056ddc9c
 // send transaction
 app.get("/sendTransaction", async (req, res) => {
   // https://speedy-nodes-nyc.moralis.io/47ef2509ed164f0635b5048e/eth/ropsten
@@ -38,19 +38,56 @@ app.get("/sendTransaction", async (req, res) => {
 
   const signer = wallet.connect(connection);
   const recipient = req.query.recipient;
-  const tx = {
-    from: wallet.address,
-    to: recipient,
-    value: ethers.utils.parseEther(req.query.amount),
-    gasPrice: gasPrice,
-    gasLimit: ethers.utils.hexlify(100000),
-    nonce: connection.getTransactionCount(wallet.address, "latest"),
-  };
-  const transaction = await signer.sendTransaction(tx);
-  console.log(transaction);
-  // const receipt = await transaction.wait();
-  // console.log(receipt);
-  res.send(transaction);
+  // const tx = {
+  //   from: wallet.address,
+  //   to: recipient,
+  //   value: ethers.utils.parseEther(req.query.amount),
+  //   gasPrice: gasPrice,
+  //   gasLimit: ethers.utils.hexlify(100000),
+  //   nonce: connection.getTransactionCount(wallet.address, "latest"),
+  // };
+  // You can also use an ENS name for the contract address
+  // const daiAddress = "dai.tokens.ethers.eth";
+
+  // Connect to the contract
+  // dai contract address
+  // var contractAddress = "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa";
+  // test contract address ( we created it for testing.. )
+  // var contractAddress = "0x814f11f6bd1a717b21849da13553d457056ddc9c";
+  var contractAddress = req.query.contractAddress;
+  var contractAbiFragment = [
+    {
+      name: "transfer",
+      type: "function",
+      inputs: [
+        {
+          name: "_to",
+          type: "address",
+        },
+        {
+          type: "uint256",
+          name: "_tokens",
+        },
+      ],
+      constant: false,
+      outputs: [],
+      payable: false,
+    },
+  ];
+  var contract = new ethers.Contract(contractAddress, contractAbiFragment, signer);
+
+  // How many tokens?
+  var numberOfDecimals = 18;
+  var numberOfTokens = ethers.utils.parseUnits(req.query.amount, numberOfDecimals);
+
+  // Send tokens
+  contract.transfer(recipient, numberOfTokens).then(function (tx) {
+    console.log(tx);
+    res.send(tx);
+  });
+  // const transaction = await signer.sendTransaction(tx);
+  // console.log(transaction);
+  // res.send(transaction);
 });
 
 app.listen(port, () => {
